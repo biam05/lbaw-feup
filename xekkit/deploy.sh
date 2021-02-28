5c6fc9fc37de4e02ba8e951c79ddd3779d887dbe
Ricardo Cardoso <ricardofdc@live.com>
2021-02-28 21:49:13 +0000

    minor changes

--- xekkit/deploy.sh
+++ xekkit/deploy.sh
@@ -4,10 +4,7 @@
 set -e
 
 DOCKER_USERNAME=lbaw2114 # Replace by your docker hub username
-DOCKER_PASSWORD=rumo_ao_20
 IMAGE_NAME=lbaw2114-piu
 
-
-echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME  --password-stdin
 docker build -t $DOCKER_USERNAME/$IMAGE_NAME .
 docker push $DOCKER_USERNAME/$IMAGE_NAME
