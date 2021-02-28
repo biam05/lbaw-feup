f32ebb6fec14e0d1d01403e0185534189a5744d5
GCandicella <guilherme.calassi@webcomum.com>
2021-02-28 19:35:05 +0000

    start docker if already exist container

--- xekkit/run.sh
+++ xekkit/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 FLAGS=""
+DOCKER_CONTAINER="lbaw2114"
 
 for opt in $@
 do
@@ -11,7 +12,16 @@ do
     esac
 done
 
-docker run $FLAGS --name lbaw2114 -it -p 8000:80 -v $PWD/html:/var/www/html lbaw2114/lbaw2114-piu
+if [ ! "$(docker ps -q -f name=$DOCKER_CONTAINER)" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=$DOCKER_CONTAINER)" ]; then
+        docker start $FLAGS $DOCKER_CONTAINER
+    else
+    # run your container
+    docker run $FLAGS --name $DOCKER_CONTAINER -it -p 8000:80 -v $PWD/html:/var/www/html lbaw2114/lbaw2114-piu
+    fi
+fi
+
+
 
 echo "\n"
 echo ">> RUNNING LBAW2114 on http://localhost:8000"
