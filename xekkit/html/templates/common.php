463b12c5d67d9e9ce580b2e0e4eb1cc4fab6cf8b
Ricardo Cardoso <ricardofdc@live.com>
2021-02-28 18:18:44 +0000

    colors overwritten in css

--- xekkit/html/templates/common.php
+++ xekkit/html/templates/common.php
@@ -15,7 +15,14 @@ function draw_header()
         <title>Xekkit</title>
         <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
         <script defer src="https://kit.fontawesome.com/0f8556fd7f.js" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="./style/style.css">     
+        <link rel="stylesheet" href="./css/primary.css">     
+        <link rel="stylesheet" href="./css/secondary.css">  
+        <link rel="stylesheet" href="./css/success.css">  
+        <link rel="stylesheet" href="./css/danger.css">  
+        <link rel="stylesheet" href="./css/warning.css">  
+        <link rel="stylesheet" href="./css/info.css">  
+        <link rel="stylesheet" href="./css/light.css">  
+        <link rel="stylesheet" href="./css/dark.css">  
     </head>
 
     <body>
