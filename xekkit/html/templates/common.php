70cc2af214b1a36e763beca572166da5ddd49778
Ricardo Cardoso <ricardofdc@live.com>
2021-03-02 10:45:59 +0000

    navbar responsiveness improved

--- xekkit/html/templates/common.php
+++ xekkit/html/templates/common.php
@@ -12,7 +12,7 @@ function draw_header()
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Xekkit</title>
+        <title>XEKKIT</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
         <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
         <script defer src="https://kit.fontawesome.com/0f8556fd7f.js" crossorigin="anonymous"></script>
@@ -37,10 +37,10 @@ function draw_header()
  */
 function draw_footer()
 { ?>
-        <nav class="navbar fixed-bottom navbar-expand-lg navbar-dark bg-dark">
+        <nav class="navbar fixed-bottom navbar-expand navbar-dark bg-dark">
             <div class="container-fluid">
                 
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <ul class="navbar-nav me-auto mb-0 ">
                         <li class="nav-item">
                             <a class="nav-link" href="../pages/about_us.php">About Us</a>
                         </li>
