02a0b20354109d1f93f5d2101ea9e1f4eff4cf55
fuscati <up201806140@fe.up.pt>
2021-03-04 14:08:06 +0000

    Post Done

--- xekkit/html/templates/post.php
+++ xekkit/html/templates/post.php
@@ -1,12 +1,15 @@
 <?php
-function draw_post()
+function draw_post($title, $description, $author, $date, $tags, $image, $comments, $votes )
 {?>
 
 <div class="card mb-3 text-white bg-light-dark">
     <div class="card-body">
-      <h5 class="card-title">Daft Punk</h5>
-      <img src="../img/daft_punk.jpeg" class="card-img-top" alt="..."> 
-      <p class="card-text">Daft Punk Break Up Annoucement image </p>
+      <h5 class="card-title" style="display:inline-block"><?=$title?></h5>
+      <button class="card-author clickable text-white"><?=$author?></button>
+      <button class="card-report clickable-big text-white"><i class="fas fa-exclamation-triangle"></i></button>
+      <span class="card-date"><?=$date?></span>
+      <img src=<?=$image?> class="card-img-top" alt="..." draggable="false"> 
+      <p class="card-text"><?=$description?></p>
     </div>
     <div class="card-footer text-muted" style="display:flex;">
       <div class="votes">
@@ -18,8 +21,18 @@ function draw_post()
         </button>
       </div>
       <footer>
-        <span>7873</span>
-        <a href="#" class="comments fas fa-comment clickable"> 420</a>
+          <span><?=$votes?></span>
+          <button  class="comments clickable" ><i class="fas fa-comment"></i> <?=$comments?></button>
+          <div class="card-tags">
+            <?php
+            foreach($tags as $tag)
+            {
+              ?>
+                <button class="clickable text-white">#<?=$tag?></button>
+              <?php
+            }
+            ?>
+          </div>
       </footer>
     </div>
 </div>
@@ -33,7 +46,7 @@ function draw_posts()
 
 <div class="container-xl">
   <?php
-    draw_post();
+    draw_post("Daft Punk", "Daft Punk Break Up Announcement image", "x/johndoe", "Jan 23, 2021", ["music", "celebreties"], "../img/daft_punk.jpeg", "58", "7873" );
   ?>
 </div>
 
@@ -42,28 +55,3 @@ function draw_posts()
 }
 ?>
 
-<!-- <div class="card mb-3 text-white bg-light-dark" style="width: 80%; margin:auto;">
-  <div class="card-body">
-    <h5 class="card-title">Daft Punk</h5>
-    <div>
-      <video controls>
-        <source src="../img/video.mp4" type="video/mp4">
-      </video>
-    </div>
-    <p class="card-text">Daft Punk Break Up Annoucement video </p>
-  </div>
-  <div class="card-footer text-muted" style="display:flex;">
-    <div class="votes">
-      <button class="clickable-big">
-        <i class="fas fa-angle-up text-white"></i>
-      </button>
-      <button class="clickable-big">
-        <i class="fas fa-angle-down text-white "></i>
-      </button>
-    </div>
-    <footer>
-      <span>7873</span>
-      <a href="#" class="comments fas fa-comment clickable">420</a>
-    </footer>
-  </div> 
-</div>-->
