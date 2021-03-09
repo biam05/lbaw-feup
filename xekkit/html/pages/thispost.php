c7ef18b64c57ae48313049c98dfc4745d46a5ec0
Beatriz Mendes <45185351+biam05@users.noreply.github.com>
2021-03-09 17:33:32 +0000

    finished comment section in the post page

--- xekkit/html/pages/thispost.php
+++ xekkit/html/pages/thispost.php
@@ -10,8 +10,17 @@ draw_nav_bar();
         <?php
             draw_post("Daft Punk", "Daft Punk Break Up Announcement image", "x/johndoe", "Jan 23, 2021", ["music", "celebreties"], "../img/daft_punk.jpeg", "58", "7873" );
         ?>
-        <div class="container m-0 p-0 comments bg-light-dark">
-            <div class="row p-3">
+        <div class="container mb-3 p-3 bg-light-dark">
+            <div class="form-floating">
+                <h3 class="text-white">Leave a Comment here</h3>
+                <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px" width="100%"></textarea>
+                <div class="col text-end mt-3">
+                    <button type="submit" class="btn btn-lg btn-primary">Submit</button>
+                </div>
+            </div>
+        </div>
+        <div class="container p-3 bg-light-dark">
+            <div class="row">
                 <div class="col-md-1">
                     <img src="../img/user.png" class="rounded-circle" alt="..." width = 50>
                 </div>
@@ -19,55 +28,73 @@ draw_nav_bar();
                     <p class="text-white text-muted p-0 m-0"><small>x/uCanadaba3 10 hours ago</small>
                         <button class="clickable-big text-muted"><i class="fas fa-exclamation-triangle"></i></button>
                     </p>        
-                    <p class="text-white">Sad moment in the music industry test test test test test test test test test test test test test test test test test test test test test test test test</p>
-                    <div class="row">
-                        <div class="col-md-1">
-                            <img src="../img/user.png" class="rounded-circle" alt="..." width = 50>
+                    <p class="text-white m-0">Sad moment in the music industry</p>
+                    <footer class="text-muted">
+                        <div class="votes">
+                            <button class="clickable-big">
+                                <i class="fas fa-angle-up text-muted"></i>
+                            </button>
+                            <button class="clickable-big">
+                                <i class="fas fa-angle-down text-muted"></i>
+                            </button>
                         </div>
-                        <div class="col-md">
-                            <p class="text-white text-muted p-0 m-0"><small>x/johndoe 10 hours ago</small>
-                                <button class="clickable-big text-muted"><i class="fas fa-exclamation-triangle"></i></button>
-                            </p>  
-                            <p class="text-white">Agreed :(</p>                              
-                        </div>
-                    </div>
-                </div>
+                        <span>15</span>
+                        <button  class="comments clickable" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fas fa-reply"></i> 1</button>
+                    </footer> 
+                </div>                 
             </div> 
-            <footer class="text-muted">
-                <div class="votes">
-                    <button class="clickable-big">
-                        <i class="fas fa-angle-up text-muted"></i>
-                    </button>
-                    <button class="clickable-big">
-                        <i class="fas fa-angle-down text-muted"></i>
-                    </button>
-                </div>
-                <span>123</span>
-                <button  class="comments clickable" ><i class="fas fa-reply"></i> 123</button>
-            </footer>                
-            <hr class="featurette-divider text-muted ml-1 mr-1">  
-            <div class="row p-3">
+            <div class="row">
+                <div class="col-md-1"></div>
                 <div class="col-md-1">
                     <img src="../img/user.png" class="rounded-circle" alt="..." width = 50>
                 </div>
                 <div class="col-md">
-                    <p class="text-white">The majority of our website structure and concept (including the name, obviously)
-                        is based on the famous website</p>
-                </div>
-            </div>            
-            <hr class="featurette-divider text-muted ml-1 mr-1">  
-            <div class="row p-3">
-                <div class="col-md-1">
-                    <img src="../img/user.png" class="rounded-circle" alt="..." width = 50>
-                </div>
-                <div class="col-md">
-                    <p class="text-white">The majority of our website structure and concept (including the name, obviously)
-                        is based on the famous website</p>
+                    <p class="text-white text-muted p-0 m-0"><small>x/johndoe 10 hours ago</small>
+                        <button class="clickable-big text-muted"><i class="fas fa-exclamation-triangle"></i></button>
+                    </p>  
+                    <p class="text-white m-0">Agreed :(</p>   
+                    <footer class="text-muted">
+                        <div class="votes">
+                            <button class="clickable-big">
+                                <i class="fas fa-angle-up text-muted"></i>
+                            </button>
+                            <button class="clickable-big">
+                                <i class="fas fa-angle-down text-muted"></i>
+                            </button>
+                        </div>
+                        <span>2</span>
+                        <button  class="comments clickable" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fas fa-reply"></i> 0</button>
+                    </footer>                            
                 </div>
             </div>
+            <hr class="featurette-divider text-muted">  
+            <div class="row p-3">
+                <div class="col-md-1">
+                    <img src="../img/user.png" class="rounded-circle" alt="..." width = 50>
+                </div>
+                <div class="col-md">
+                    <p class="text-white text-muted p-0 m-0"><small>x/randomdude 2 days ago</small>
+                        <button class="clickable-big text-muted"><i class="fas fa-exclamation-triangle"></i></button>
+                    </p>        
+                    <p class="text-white m-0">Never really liked them tbh</p>
+                    <footer class="text-muted">
+                        <div class="votes">
+                            <button class="clickable-big">
+                                <i class="fas fa-angle-up text-muted"></i>
+                            </button>
+                            <button class="clickable-big">
+                                <i class="fas fa-angle-down text-muted"></i>
+                            </button>
+                        </div>
+                        <span>1</span>
+                        <button  class="comments clickable" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fas fa-reply"></i> 0</button>
+                    </footer> 
+                </div>                 
+            </div>          
+            
         </div>   
     </div>
-    <aside class="bd-aside mt-4">
+    <aside class="bd-aside">
         <div class="card align-items-center text-center text-white bg-light-dark" style="width: 18rem;">
             <img src="../img/user.png" class="card-img-top" alt="user image" style="width: 10rem;">
             <div class="card-body"> 
@@ -82,6 +109,28 @@ draw_nav_bar();
     </aside>
     
 </div>
+
+
+<!-- Modal -->
+<div class="modal fade text-white" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog text-white">
+      <div class="modal-content bg-light-dark text-white">
+        <div class="modal-header">
+          <h5 class="modal-title text-white" id="exampleModalLabel">Reply</h5>
+          <button type="button" class="btn-close btn-close-white " data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            <div class="form-floating">
+                <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px" width="100%"></textarea>
+                <div class="col text-end mt-3">
+                    <button type="submit" class="btn btn-lg btn-primary">Submit</button>
+                </div>
+            </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
 
 <?php
 draw_footer();
