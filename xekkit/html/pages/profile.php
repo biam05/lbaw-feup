0b4367544d01dedb1f85a7a6d316ca1ae303c10c
Guilherme Calassi <guilherme@yourmove.com.br>
2021-03-12 00:51:24 +0000

    my profile and my posts

--- xekkit/html/pages/profile.php
+++ xekkit/html/pages/profile.php
@@ -1,8 +1,11 @@
 <?php
-    include_once('../templates/common.php');    
-    include_once('../templates/post.php'); 
-    draw_header();
-    draw_nav_bar();
+include_once('../templates/common.php');
+include_once('../templates/post.php');
+draw_header();
+draw_nav_bar();
+
+$is_myprofile = true;
+$is_logged = true;
 ?>
 
 <section class="container page profile">
@@ -11,116 +14,124 @@
             <div class="container">
                 <div class="row align-items-start">
                     <div class="col">
-                        <h5 class="card-title"> <i class="fas fa-check"></i> x/johndoe</h5> 
+                        <h5 class="card-title"><i class="fas fa-check"></i> x/johndoe</h5>
                     </div>
-                    <div class="col">                
-                        <a href="#" class="btn btn-primary">Follow</a>
-                    </div>
-                    <div class="col">                
-                    <button type="button" class="profile-report clickable-big text-white" data-bs-toggle="modal" data-bs-target="#reportModal">
-                        <i class="fas fa-exclamation-triangle"></i>
-                        </button>
-                            <?php draw_report_modal()?>      
-
-                    </div>
-                </div>  
+                    <?php if (!$is_myprofile) { ?>
+                        <?php if ($is_logged) { ?>
+                            <div class="col">
+                                <a href="#" class="btn btn-primary">Follow</a>
+                            </div>
+                        <?php } ?>
+                        <div class="col">
+                            <button type="button" class="profile-report clickable-big text-white" data-bs-toggle="modal" data-bs-target="#reportModal">
+                                <i class="fas fa-exclamation-triangle"></i>
+                            </button>
+                            <?php draw_report_modal() ?>
+                        </div>
+                    <?php } else { ?>
+                        <?php if ($is_logged) { ?>
+                            <div class="col">
+                                <a href="editprofile.php" class="btn btn-primary">Edit</a>
+                            </div>
+                        <?php } ?>
+                    <?php } ?>
+                </div>
                 <div class="row align-items-center">
                     <div class="col">
                         <img src="../img/user.png" class="card-img-top" alt="user image" style="width: 10rem;">
                     </div>
-                    <div class="col">                
+                    <div class="col">
                         <div class="card-text">
                             <p>Reputation</p>
-                            <h2>254,789</h4>
-                        </p>    
-                    </div>
-                </div>           
-            </div>          
-        </div>
-        <div class="card" style="width: 30em;">
-            <div class="card-body">
-                <h5 class="card-title">Description</h5>
-                <p class="card-text">My name is John and I'm a partner example user for this framewhire. My posts are really about any trending topic.</p>
-            </div>
-        </div>
-    </section>
-    <nav>
-    <ul class="nav nav-pills mb-3 text-white bg-light-dark" id="pills-tab" role="tablist">
-        <li class="nav-item " role="presentation">
-            <button class="nav-link active text-white" id="pills-trending-tab" data-bs-toggle="pill" data-bs-target="#pills-trending" type="button" role="tab" aria-controls="pills-trending" aria-selected="true">Home</button>
-        </li>
-        <li class="nav-item" role="presentation">
-            <button class="nav-link text-white" id="pills-top-tab" data-bs-toggle="pill" data-bs-target="#pills-top" type="button" role="tab" aria-controls="pills-top" aria-selected="false">Top</button>
-        </li>
-        <li class="nav-item" role="presentation">
-            <button class="nav-link text-white" id="pills-new-tab" data-bs-toggle="pill" data-bs-target="#pills-new" type="button" role="tab" aria-controls="pills-new" aria-selected="false">New</button>
-        </li>    
-        <li class="nav-item" role="presentation">
-            <button class="nav-link text-white" id="pills-following-tab" data-bs-toggle="pill" data-bs-target="#pills-following" type="button" role="tab" aria-controls="pills-following" aria-selected="false">Following</button>
-        </li>
-    </ul>
-    <div class="tab-content" id="pills-tabContent">
-        <div class="tab-pane fade show active" id="pills-trending" role="tabpanel" aria-labelledby="pills-trending-tab">
-            <?php
-                draw_posts();
-            ?>
-        </div>
-        <div class="tab-pane fade" id="pills-top" role="tabpanel" aria-labelledby="pills-top-tab">
-            <?php
-                draw_posts();
-            ?>
-        </div>
-        <div class="tab-pane fade" id="pills-new" role="tabpanel" aria-labelledby="pills-new-tab">
-            <?php
-                draw_posts();
-            ?>
-        </div>
-        <div class="tab-pane fade" id="pills-following" role="tabpanel" aria-labelledby="pills-following-tab">
-            <div class="container d-flex alignt-items-center text-center">    
-                <div class="row">
-                    <div class="col-sm">
-                        <div class="card align-items-center text-white bg-light-dark" style="width: 18rem;">
-                            <img src="../img/user.png" class="card-img-top" alt="user image" style="width: 10rem;">
-                            <div class="card-body"> 
-                                <button class="card-title clickable text-white">x/someone</button>              
-                                <div class="card-text">
-                                    <p>Reputation</p>
-                                    <h2>254,789</h4>
-                                </div>                   
-                            </div>
+                            <h2>254,789</h2>
                         </div>
-                    </div>
-                    <div class="col-sm">   
-                        <div class="card align-items-center text-white bg-light-dark" style="width: 18rem;">
-                            <img src="../img/user.png" class="card-img-top" alt="user image" style="width: 10rem;">
-                            <div class="card-body"> 
-                                <button class="card-title clickable text-white">x/someone</button>              
-                                <div class="card-text">
-                                    <p>Reputation</p>
-                                    <h2>254,789</h4>
-                                </div>                   
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm">
-                        <div class="card align-items-center text-white bg-light-dark" style="width: 18rem;">
-                            <img src="../img/user.png" class="card-img-top" alt="user image" style="width: 10rem;">
-                            <div class="card-body"> 
-                                <button class="card-title clickable text-white">x/someone</button>              
-                                <div class="card-text">
-                                    <p>Reputation</p>
-                                    <h2>254,789</h4>
-                                </div>                   
-                            </div>
-                        </div>
-
                     </div>
                 </div>
-            </div>            
+            </div>
+            <div class="card" style="width: 30em;">
+                <div class="card-body">
+                    <h5 class="card-title">Description</h5>
+                    <p class="card-text">My name is John and I'm a partner example user for this framewhire. My posts are really about any trending topic.</p>
+                </div>
+            </div>
+    </section>
+    <nav>
+        <ul class="nav nav-pills mb-3 text-white bg-light-dark" id="pills-tab" role="tablist">
+            <li class="nav-item " role="presentation">
+                <button class="nav-link active text-white" id="pills-trending-tab" data-bs-toggle="pill" data-bs-target="#pills-trending" type="button" role="tab" aria-controls="pills-trending" aria-selected="true">Home</button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link text-white" id="pills-top-tab" data-bs-toggle="pill" data-bs-target="#pills-top" type="button" role="tab" aria-controls="pills-top" aria-selected="false">Top</button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link text-white" id="pills-new-tab" data-bs-toggle="pill" data-bs-target="#pills-new" type="button" role="tab" aria-controls="pills-new" aria-selected="false">New</button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link text-white" id="pills-following-tab" data-bs-toggle="pill" data-bs-target="#pills-following" type="button" role="tab" aria-controls="pills-following" aria-selected="false">Following</button>
+            </li>
+        </ul>
+        <div class="tab-content" id="pills-tabContent">
+            <div class="tab-pane fade show active" id="pills-trending" role="tabpanel" aria-labelledby="pills-trending-tab">
+                <?php
+                draw_posts($is_myprofile);
+                ?>
+            </div>
+            <div class="tab-pane fade" id="pills-top" role="tabpanel" aria-labelledby="pills-top-tab">
+                <?php
+                draw_posts($is_myprofile);
+                ?>
+            </div>
+            <div class="tab-pane fade" id="pills-new" role="tabpanel" aria-labelledby="pills-new-tab">
+                <?php
+                draw_posts($is_myprofile);
+                ?>
+            </div>
+            <div class="tab-pane fade" id="pills-following" role="tabpanel" aria-labelledby="pills-following-tab">
+                <div class="container d-flex alignt-items-center text-center">
+                    <div class="row">
+                        <div class="col-sm">
+                            <div class="card align-items-center text-white bg-light-dark" style="width: 18rem;">
+                                <img src="../img/user.png" class="card-img-top" alt="user image" style="width: 10rem;">
+                                <div class="card-body">
+                                    <button class="card-title clickable text-white">x/someone</button>
+                                    <div class="card-text">
+                                        <p>Reputation</p>
+                                        <h2>254,789</h2>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm">
+                            <div class="card align-items-center text-white bg-light-dark" style="width: 18rem;">
+                                <img src="../img/user.png" class="card-img-top" alt="user image" style="width: 10rem;">
+                                <div class="card-body">
+                                    <button class="card-title clickable text-white">x/someone</button>
+                                    <div class="card-text">
+                                        <p>Reputation</p>
+                                        <h2>254,789</h2>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm">
+                            <div class="card align-items-center text-white bg-light-dark" style="width: 18rem;">
+                                <img src="../img/user.png" class="card-img-top" alt="user image" style="width: 10rem;">
+                                <div class="card-body">
+                                    <button class="card-title clickable text-white">x/someone</button>
+                                    <div class="card-text">
+                                        <p>Reputation</p>
+                                        <h2>254,789</h2>
+                                    </div>
+                                </div>
+                            </div>
+                        
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
 </section>
 
-<?php  
-    draw_footer();
+<?php
+draw_footer();
 ?>
--- xekkit/html/templates/post.php
+++ xekkit/html/templates/post.php
@@ -6,108 +6,151 @@ draw_toast();
 
 
 <?php
-function draw_post($title, $description, $author, $date, $tags, $image, $comments, $votes )
-{?>
-
-<div class="card mb-3 text-white bg-light-dark">
-    <div class="card-body">
-      <h5 class="card-title" style="display:inline-block"><?=$title?></h5>
-      <button class="card-author clickable text-white" onclick="document.location='../pages/profile.php'"><?=$author?></button>
-     
-      <button type="button" id="toastbtn" class="card-report clickable-big text-white" data-bs-toggle="modal" data-bs-target="#reportModal">
-      <i class="fas fa-exclamation-triangle"></i>
-      </button>
-         <?php draw_report_modal()?>      
-
-      <button class="card-date clickable text-decoration-none text-white" onclick="document.location='../pages/thispost.php'"><?=$date?></button>
-      <img src=<?=$image?> class="card-img-top" alt="..." draggable="false"> 
-      <p class="card-text">
-          <?php 
-        echo substr($description,0,150);
-        if(strlen($description)>150)
-        {
-          echo '...';
-        }
-        ?>
-      </p>
-    </div>
-    <div class="card-footer text-muted" style="display:flex;">
-      
-      <footer>
-      <div class="card-tags">
-
-               
-              <?php
-              foreach($tags as $tag)
-              {
-                ?>
-                  <button class="clickable text-white">#<?=$tag?></button>
-                <?php
-              }
-              ?>
-            </div>
-        <div class="votes">
-          <button class="clickable-big">
-            <i class="fas fa-angle-up text-white"></i>
-          </button>
-          <button class="clickable-big">
-            <i class="fas fa-angle-down text-white "></i>
-          </button>
-        </div>
-            <span><?=$votes?></span>
-            <button  class="comments clickable" ><i class="fas fa-comment"></i> <?=$comments?></button>
+function draw_post($title, $description, $author, $date, $tags, $image, $comments, $votes, $is_mine = false)
+{
+    ?>
+    
+    <div class="card mb-3 text-white bg-light-dark">
+        <div class="card-body">
+            <h5 class="card-title" style="display:inline-block"><?= $title ?></h5>
+            <button class="card-author clickable text-white" onclick="document.location='../pages/profile.php'"><?= $author ?></button>
             
-        </footer>
-      </div>
-  </div>
-
-
-
-<?php
+            <?php if ($is_mine) { ?>
+                <button type="button" class="card-report clickable-big text-white" data-bs-toggle="modal" data-bs-target="#deletePostModal">
+                    <i class="fas fa-times"></i>
+                </button>
+                <?php draw_delete_post(); ?>
+            <?php } else { ?>
+                <button type="button" id="toastbtn" class="card-report clickable-big text-white" data-bs-toggle="modal" data-bs-target="#reportModal">
+                    <i class="fas fa-exclamation-triangle"></i>
+                </button>
+                <?php draw_report_modal() ?>
+            <?php } ?>
+            
+            <button class="card-date clickable text-decoration-none text-white" onclick="document.location='../pages/thispost.php'"><?= $date ?></button>
+            <img src=<?= $image ?> class="card-img-top" alt="..." draggable="false">
+            <p class="card-text">
+                <?php
+                echo substr($description, 0, 150);
+                if (strlen($description) > 150) {
+                    echo '...';
+                }
+                ?>
+            </p>
+        </div>
+        <div class="card-footer text-muted" style="display:flex;">
+            
+            <footer>
+                <div class="card-tags">
+                    
+                    
+                    <?php
+                    foreach ($tags as $tag) {
+                        ?>
+                        <button class="clickable text-white">#<?= $tag ?></button>
+                        <?php
+                    }
+                    ?>
+                </div>
+                <div class="votes">
+                    <button class="clickable-big">
+                        <i class="fas fa-angle-up text-white"></i>
+                    </button>
+                    <button class="clickable-big">
+                        <i class="fas fa-angle-down text-white "></i>
+                    </button>
+                </div>
+                <span><?= $votes ?></span>
+                <button class="comments clickable"><i class="fas fa-comment"></i> <?= $comments ?></button>
+            
+            </footer>
+        </div>
+    </div>
+    
+    
+    <?php
 }
 
-function draw_posts()
-{?>
-
-<div class="container-xl">
-  
-  <?php
-    draw_post("Daft Punk", "Daft Punk Break Up Announcement image", "x/johndoe", "Jan 23, 2021", ["music", "celebreties"], "../img/daft_punk.jpeg", "58", "7873" );
-    draw_post("Daft Punk", "This is a test to see the size of a description needed to trigger the reticências. So far this has 100 characteres. Is it good enough? Short maybe? Long maybe? One could never know writing text on the text editor, so I continue to write until I can consider this a big Post. Maybe I should just paste some Lorem Ipsum shit, but I don't really want to, this is more genuine", "x/johndoe", "Jan 23, 2021", ["music", "celebreties"], "../img/daft_punk.jpeg", "58", "7873" );
-  ?>
-</div>
-
-
-<?php
+function draw_posts($is_mine)
+{
+    ?>
+    
+    <div class="container-xl">
+        
+        <?php
+        draw_post("Daft Punk", "Daft Punk Break Up Announcement image", "x/johndoe", "Jan 23, 2021",
+            ["music", "celebreties"], "../img/daft_punk.jpeg", "58", "7873", $is_mine);
+        draw_post("Daft Punk",
+            "This is a test to see the size of a description needed to trigger the reticências. So far this has 100 characteres. Is it good enough? Short maybe? Long maybe? One could never know writing text on the text editor, so I continue to write until I can consider this a big Post. Maybe I should just paste some Lorem Ipsum shit, but I don't really want to, this is more genuine",
+            "x/johndoe", "Jan 23, 2021", ["music", "celebreties"], "../img/daft_punk.jpeg", "58", "7873", $is_mine);
+        ?>
+    </div>
+    
+    
+    <?php
 }
+
 ?>
 
 <?php function draw_report_modal()
-{?>
-<div class="modal fade text-white" id="reportModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog text-white">
-      <div class="modal-content bg-light-dark text-white">
-        <div class="modal-header">
-          <h5 class="modal-title text-white" id="Report-modal-label">Report form</h5>
-          <button type="button" class="btn-close btn-close-white " data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-            <div class="mb-3">
-              <label for="Report-modal-description" class="form-label">Reason to Report</label>
-              <span id="Report-modal-description" class="input form-control" role="textbox" rows="3" contenteditable aria-multiline="true">
-              </span>
+{
+    ?>
+    <div class="modal fade text-white" id="reportModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog text-white">
+            <div class="modal-content bg-light-dark text-white">
+                <div class="modal-header">
+                    <h5 class="modal-title text-white" id="Report-modal-label">Report form</h5>
+                    <button type="button" class="btn-close btn-close-white " data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="mb-3">
+                        <label for="Report-modal-description" class="form-label">Reason to Report</label>
+                        <span id="Report-modal-description" class="input form-control" role="textbox" rows="3" contenteditable aria-multiline="true">
+                        </span>
+                    </div>
+                    
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            
             </div>
-
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="submit" class="btn btn-primary">Submit</button>
-          </form>
         </div>
-
-      </div>
     </div>
-  </div>
-
-<?php
+    
+    <?php
 }
+
+?>
+
+<?php function draw_delete_post()
+{
+    ?>
+    <div class="modal fade text-white" id="deletePostModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog text-white">
+            <div class="modal-content bg-light-dark text-white">
+                <div class="modal-header">
+                    <h5 class="modal-title text-white" id="Report-modal-label">Delete Posts</h5>
+                    <button type="button" class="btn-close btn-close-white " data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="mb-3">
+                        <label for="Report-modal-description" class="form-label">Confim by typing your password</label>
+                        <span id="Report-modal-description" class="input form-control" role="textbox" rows="3" contenteditable aria-multiline="true">
+                        </span>
+                    </div>
+                    
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            
+            </div>
+        </div>
+    </div>
+    
+    <?php
+}
+
 ?>
 
