569fae8b19a75bfdcec5988ca008a8a8f0d44db2
Guilherme Calassi <guilherme@yourmove.com.br>
2021-03-13 19:18:00 +0000

    check null addeventlistener

--- xekkit/html/js/main.js
+++ xekkit/html/js/main.js
@@ -7,14 +7,16 @@ preventer.addEventListener("click", (e) => {
 });
 
 const tagforminput = document.getElementById("News-modal-tags");
-tagforminput.addEventListener("keydown", function(event) {
-    if (event.key === "Enter") {
-        event.preventDefault();
-        let parentTags = document.getElementById("parentTags");
-        let span = document.createElement("span")
-        span.className = "badge badge-primary";
-        span.append(tagforminput.value)
-        parentTags.append(span)
-        tagforminput.value = '';
-    }
-});
+if (tagforminput !== undefined && tagforminput !== null) {
+    tagforminput.addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            let parentTags = document.getElementById("parentTags");
+            let span = document.createElement("span")
+            span.className = "badge badge-primary";
+            span.append(tagforminput.value)
+            parentTags.append(span)
+            tagforminput.value = '';
+        }
+    });
+}
