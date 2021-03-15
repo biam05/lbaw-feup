4a9dbb3b76fab4be1f20dd3bd983282a06d06d73
Ricardo Cardoso <up201604686@fe.up.pt>
2021-03-15 16:32:01 +0000

    minor change

--- xekkit/html/pages/banned.php
+++ xekkit/html/pages/banned.php
@@ -16,44 +16,44 @@ draw_nav_bar();
 </div>
 
 <div class="modal fade text-white" id="unbanAppeal" tabindex="-1" aria-labelledby="newPostLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content bg-light-dark">
-        <div class="modal-header">
-          <h5 class="modal-title text-white" id="newPostLabel">Unban Appeal</h5>
-          <button type="button" class="btn-close btn-close-white " data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-            <p class="text-white">Fill out the form so the moderators can review your unban appeal</p>
-          <form>
-            <div class="mb-3">
-                <input type="email" class="form-control" id="inputEmail" placeholder="Email" value="" required>
-                <div class="invalid-feedback">
-                    Email already in use or invalid format.
-                </div>
-            </div>    
-            <div class="mb-3">
-                <input type="password" value="" class="form-control" id="inputPassword" placeholder="Password">
-                <div class="invalid-feedback">
-                    Your password must be at least 8 characters long, contain letters and numbers.
-                </div>
-            </div>
-            <div class="mb-3">
-              <label for="News-modal-description" class="form-label">Unban Appeal</label>
-              <span id="News-modal-description" class="input form-control" role="textbox" rows="3" contenteditable aria-multiline="true">
-              </span>
-            </div>
-            <div class="mb-3">
-              <div class="container" id="file-display-area">
-              </div>
-            </div>
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="submit" class="btn btn-primary" onclick="location.href='banned.php';">Submit</button>
-          </form>
-        </div>
-
+  <div class="modal-dialog">
+    <div class="modal-content bg-light-dark">
+      <div class="modal-header">
+        <h5 class="modal-title text-white" id="newPostLabel">Unban Appeal</h5>
+        <button type="button" class="btn-close btn-close-white " data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
+      <div class="modal-body">
+          <p class="text-white">Fill out the form so the moderators can review your unban appeal</p>
+        <form>
+          <div class="mb-3">
+              <input type="email" class="form-control" id="inputEmail" placeholder="Email" value="" required>
+              <div class="invalid-feedback">
+                  Email already in use or invalid format.
+              </div>
+          </div>    
+          <div class="mb-3">
+              <input type="password" value="" class="form-control" id="inputPassword" placeholder="Password">
+              <div class="invalid-feedback">
+                  Your password must be at least 8 characters long, contain letters and numbers.
+              </div>
+          </div>
+          <div class="mb-3">
+            <label for="News-modal-description" class="form-label">Unban Appeal</label>
+            <span id="News-modal-description" class="input form-control" role="textbox" rows="3" contenteditable aria-multiline="true">
+            </span>
+          </div>
+          <div class="mb-3">
+            <div class="container" id="file-display-area">
+            </div>
+          </div>
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="submit" class="btn btn-primary" onclick="location.href='banned.php';">Submit</button>
+        </form>
+      </div>
+
     </div>
   </div>
+</div>
 
-  <?php
-  draw_footer();
+<?php
+draw_footer();
