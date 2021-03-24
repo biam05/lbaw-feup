18e4231901398d355318a79dd1438d51c4c0a7a0
Ricardo Cardoso <up201604686@fe.up.pt>
2021-03-24 15:56:07 +0000

    updated sql

--- xekkit/sql/setup.sql
+++ xekkit/sql/setup.sql
@@ -5,7 +5,6 @@ DROP TABLE IF EXISTS "unban_appeal" CASCADE;
 DROP TABLE IF EXISTS "partner_request" CASCADE;
 DROP TABLE IF EXISTS "report_content" CASCADE;
 DROP TABLE IF EXISTS "report_user" CASCADE; 
-DROP TABLE IF EXISTS "status" CASCADE;
 DROP TABLE IF EXISTS "request" CASCADE;
 DROP TABLE IF EXISTS "news_tag" CASCADE;
 DROP TABLE IF EXISTS "news" CASCADE;
@@ -16,133 +15,226 @@ DROP TABLE IF EXISTS "comment" CASCADE;
 DROP TABLE IF EXISTS "follow" CASCADE;
 DROP TABLE IF EXISTS "user" CASCADE;
 
-DROP TYPE IF EXISTS gender_type CASCADE;
-DROP TYPE IF EXISTS status_type CASCADE;
+DROP TYPE IF EXISTS GENDER_TYPE CASCADE;
+DROP TYPE IF EXISTS STATUS_TYPE CASCADE;
 
-CREATE TYPE gender_type AS ENUM('m','f','n');
-CREATE TYPE status_type AS ENUM('aproved', 'rejected');
+CREATE TYPE GENDER_TYPE AS ENUM('m','f','n');
+CREATE TYPE STATUS_TYPE AS ENUM('aproved', 'rejected');
 
 CREATE TABLE "user"(
-    id SERIAL PRIMARY KEY,
+    id GENERATED ALWAYS AS IDENTITY,
     email TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL,
     description TEXT,
     photo TEXT,
     birthdate DATE NOT NULL,
-    gender gender_type NOT NULL,
+    gender GENDER_TYPE NOT NULL,
     reputation INTEGER NOT NULL DEFAULT 0 CHECK (reputation >=0) ,
     is_moderator BOOLEAN NOT NULL DEFAULT false,
+    is_partner BOOLEAN NOT NULL DEFAULT false,
     is_banned BOOLEAN NOT NULL DEFAULT false,
-    is_partner BOOLEAN NOT NULL DEFAULT false
+    is_deleted BOOLEAN NOT NULL DEFALT false,
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE "follow"(
-    id SERIAL PRIMARY KEY,
-    follower_id INTEGER NOT NULL references "user" (id) ON UPDATE CASCADE,
-    user_id INTEGER NOT NULL references "user" (id) ON UPDATE CASCADE
+    id GENERATED ALWAYS AS IDENTITY,
+    follower_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    PRIMARY KEY(id),
+    CONSTRAINT fk_follower_id
+        FOREIGN KEY(follower_id) 
+            REFERENCES "user"(id)
+            ON DELETE CASCADE,
+    CONSTRAINT fk_user_id
+        FOREIGN KEY(user_id) 
+            REFERENCES "user"(id)
+            ON DELETE CASCADE,
 );
 
 CREATE TABLE "ban"(
-    id SERIAL PRIMARY KEY,
-    start timestamp WITH TIME zone DEFAULT now() NOT NULL,
+    id GENERATED ALWAYS AS IDENTITY,
+    start timestamp WITH TIME ZONE DEFAULT now() NOT NULL,
     "end" timestamp DEFAULT NULL,
     reason TEXT,
-    moderator_id INTEGER NOT NULL REFERENCES "user" (id) ON UPDATE CASCADE, 
-    /*CHECK user.is_moderator == true */
-    id_user INTEGER NOT NULL REFERENCES "user" (id) ON UPDATE CASCADE
+    moderator_id INTEGER NOT NULL, /*CHECK user.is_moderator == true with triggers*/
+    user_id INTEGER NOT NULL,
+    PRIMARY KEY(id),
+    CONSTRAINT fk_moderator_id
+        FOREIGN KEY(moderator_id) 
+            REFERENCES "user"(id)
+            ON DELETE CASCADE,
+     CONSTRAINT fk_user_id
+        FOREIGN KEY(user_id) 
+	        REFERENCES "user"(id)
+	        ON DELETE CASCADE
+        
 );
 
 CREATE TABLE "content" (
-    id SERIAL PRIMARY KEY,
-    author_id INTEGER REFERENCES "user" (id) ON UPDATE CASCADE,
+    id GENERATED ALWAYS AS IDENTITY,
+    author_id INTEGER NOT NULL,
     body TEXT NOT NULL,
-    "date" TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL
+    "date" TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL,
+    PRIMARY KEY(id),
+    CONSTRAINT fk_author_id
+        FOREIGN KEY(author_id) 
+	        REFERENCES "user"(id)
+	        ON DELETE CASCADE
 );
 
 CREATE TABLE "tag" (
-    id SERIAL PRIMARY KEY,
-    "name" TEXT NOT NULL UNIQUE
+    id GENERATED ALWAYS AS IDENTITY,
+    "name" TEXT NOT NULL UNIQUE,
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE "news" (
-    id SERIAL PRIMARY KEY,
-    content_id INTEGER NOT NULL REFERENCES "content" (id) ON UPDATE CASCADE,
-    title TEXT NOT NULL,    
-    trending_score integer CHECK ((trending_score > 0))
+    id GENERATED ALWAYS AS IDENTITY,
+    content_id INTEGER NOT NULL,
+    title TEXT NOT NULL,
+    trending_score INTEGER CHECK (trending_score >= 0),
+    PRIMARY KEY(id),
+    CONSTRAINT fk_content_id
+        FOREIGN KEY(content_id) 
+	        REFERENCES "content"(id)
+	        ON DELETE CASCADE
+    
 );
 
 CREATE TABLE "news_tag" (
-    news_id INTEGER NOT NULL REFERENCES "news" (id) ON UPDATE CASCADE,
-    tag_id INTEGER NOT NULL REFERENCES "tag" (id) ON UPDATE CASCADE
+    news_id INTEGER,
+    tag_id INTEGER,
+    PRIMARY KEY(news_id, tag_id),
+    CONSTRAINT fk_news_id
+        FOREIGN KEY(news_id) 
+	        REFERENCES "news"(id)
+	        ON DELETE CASCADE,
+    CONSTRAINT fk_tag_id
+        FOREIGN KEY(tag_id) 
+	        REFERENCES "tag"(id)
+	        ON DELETE CASCADE
 );
 
 CREATE TABLE "comment" (
-    id SERIAL PRIMARY KEY,
+    id GENERATED ALWAYS AS IDENTITY,
     content_id INTEGER NOT NULL REFERENCES "content" (id) ON UPDATE CASCADE,
     news_id INTEGER NOT NULL REFERENCES "news" (id) ON UPDATE CASCADE,
-    reply_to_id INTEGER REFERENCES "comment" (id) ON UPDATE CASCADE
+    reply_to_id INTEGER REFERENCES "comment" (id) ON UPDATE CASCADE,
+    PRIMARY KEY(id),
+    CONSTRAINT fk_content_id
+        FOREIGN KEY(content_id) 
+	        REFERENCES "content"(id)
+	        ON DELETE CASCADE,
+    CONSTRAINT fk_news_id
+        FOREIGN KEY(news_id) 
+	        REFERENCES "news"(id)
+	        ON DELETE CASCADE,
+    CONSTRAINT fk_reply_to_id
+        FOREIGN KEY(reply_to_id) 
+	        REFERENCES "comment"(id)
+	        ON DELETE CASCADE
 );
 
 CREATE TABLE "request" (
-   id SERIAL PRIMARY KEY,
-   from_id INTEGER REFERENCES "user" (id) ON UPDATE CASCADE,
-   reason TEXT NOT NULL
-);
-
-CREATE TABLE "status" (
-   id SERIAL PRIMARY KEY,
-   request_id INTEGER NOT NULL REFERENCES "request" (id) ON UPDATE CASCADE,
-   moderator_id INTEGER REFERENCES "user" (id) ON UPDATE CASCADE, /* CHECK moderator_id.is_moderator == true */
-   "value" status_type,
-   "date" TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL
-);
+   id GENERATED ALWAYS AS IDENTITY,
+   from_id INTEGER NOT NULL,
+   moderator_id INTEGER NOT NULL, /* CHECK moderator_id.is_moderator == true WITH TRIGGERS*/
+   reason TEXT NOT NULL,
+   creation_date TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL,
+   "status" STATUS_TYPE,
+   revision_date TIMESTAMP WITH TIME ZONE,
+   PRIMARY KEY(id),
+   CONSTRAINT fk_from_id
+        FOREIGN KEY(from_id) 
+	        REFERENCES "user"(id)
+	        ON DELETE CASCADE,
+    CONSTRAINT fk_moderator_id
+        FOREIGN KEY(moderator_id) 
+	        REFERENCES "user"(id)
+	        ON DELETE CASCADE
+); 
 
 CREATE TABLE "report_user" (
-    id SERIAL PRIMARY KEY,
-    request_id INTEGER NOT NULL REFERENCES "request" (id) ON UPDATE CASCADE,
-    to_user INTEGER NOT NULL REFERENCES "user" (id) ON UPDATE CASCADE,
-    "date" TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL
+    request_id INTEGER NOT NULL,
+    to_user_id INTEGER NOT NULL,
+    PRIMARY KEY(request_id),
+    CONSTRAINT fk_request_id
+        FOREIGN KEY(request_id) 
+	        REFERENCES "request"(id)
+	        ON DELETE CASCADE,
+    CONSTRAINT fk_to_user_id
+        FOREIGN KEY(to_user_id) 
+	        REFERENCES "user"(id)
+	        ON DELETE CASCADE
 );
 
 CREATE TABLE "report_content" (
-    id SERIAL PRIMARY KEY,
-    request_id INTEGER NOT NULL REFERENCES "request" (id) ON UPDATE CASCADE,
-    to_content INTEGER NOT NULL REFERENCES "content" (id) ON UPDATE CASCADE,
-    "date" TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL
+    request_id INTEGER NOT NULL,
+    to_content_id INTEGER NOT NULL,
+    PRIMARY KEY(request_id),
+    CONSTRAINT fk_request_id
+        FOREIGN KEY(request_id) 
+	        REFERENCES "request"(id)
+	        ON DELETE CASCADE,
+    CONSTRAINT fk_to_content_id
+        FOREIGN KEY(to_content_id) 
+	        REFERENCES "content"(id)
+	        ON DELETE CASCADE
 );
 
 CREATE TABLE "partner_request" (
-    id SERIAL PRIMARY KEY,
-    request_id INTEGER NOT NULL REFERENCES "request" (id) ON UPDATE CASCADE,
-    "date" TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL
+    request_id INTEGER NOT NULL,
+    PRIMARY KEY(request_id),
+    CONSTRAINT fk_request_id
+        FOREIGN KEY(request_id) 
+	        REFERENCES "request"(id)
+	        ON DELETE CASCADE
 );
 
 CREATE TABLE "unban_appeal" (
-    id SERIAL PRIMARY KEY,
-    request_id INTEGER NOT NULL REFERENCES "request" (id) ON UPDATE CASCADE,
-    ban_id INTEGER NOT NULL REFERENCES "ban" (id) ON UPDATE CASCADE,
-    "date" TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL
+    request_id INTEGER NOT NULL,
+    ban_id INTEGER NOT NULL,
+    PRIMARY KEY(request_id),
+    CONSTRAINT fk_request_id
+        FOREIGN KEY(request_id) 
+	        REFERENCES "request"(id)
+	        ON DELETE CASCADE,
+    CONSTRAINT fk_ban_id
+        FOREIGN KEY(ban_id) 
+	        REFERENCES "ban"(id)
+	        ON DELETE CASCADE
 );
 
 CREATE TABLE "vote" (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL REFERENCES "user" (id) ON UPDATE CASCADE,
-    content_id INTEGER NOT NULL REFERENCES "content" (id) ON UPDATE CASCADE /*CHECK content.author_id != user_id */,
-    value INTEGER NOT NULL
+    user_id INTEGER,
+    content_id INTEGER, /*CHECK content.author_id != user_id */
+    value INTEGER NOT NULL,
+    PRIMARY KEY(user_id, content_id),
+    CONSTRAINT fk_user_id
+        FOREIGN KEY(user_id) 
+	        REFERENCES "user"(id)
+	        ON DELETE CASCADE,
+    CONSTRAINT fk_content_id
+        FOREIGN KEY(content_id) 
+	        REFERENCES "content"(id)
+	        ON DELETE CASCADE
 );
 
-CREATE TABLE "notification" (
-    id SERIAL PRIMARY KEY,
+CREATE TABLE "notification" ( /*not done, will change to composition*/
+    id GENERATED ALWAYS AS IDENTITY,
     user_id INTEGER NOT NULL REFERENCES "user" (id) ON UPDATE CASCADE,
     vote_id INTEGER NOT NULL REFERENCES "vote" (id) ON UPDATE CASCADE,
     comment_id INTEGER NOT NULL REFERENCES "comment" (id) ON UPDATE CASCADE,
     is_new BOOLEAN DEFAULT true,
-    CHECK ((vote_id IS NULL) != (comment_id IS NULL))
+    CHECK (vote_id IS NULL) != (comment_id IS NULL),
+    PRIMARY KEY(id),
 );
 
 
 CREATE TABLE "faq" (
-    id SERIAL PRIMARY KEY,
-    quastion TEXT NOT NULL UNIQUE,
-    answer TEXT NOT NULL
+    id GENERATED ALWAYS AS IDENTITY,
+    question TEXT NOT NULL UNIQUE,
+    answer TEXT NOT NULL,
+    PRIMARY KEY(id)
 );
