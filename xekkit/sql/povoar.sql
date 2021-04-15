804b81eb47b4579cb9deb58d4c1f4101ad15c177
fuscati <up201806140@fe.up.pt>
2021-04-15 16:56:51 +0100

    mais povoaçao da bd. pls send help

--- xekkit/sql/povoar.sql
+++ xekkit/sql/povoar.sql
@@ -1,23 +1,23 @@
-insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('mpitchers0', 'lpriestner0@tiny.cc', '13oVi9u1gN', 'morph distributed schemas', png, '02/20/1922', 'f', '01532', false, false, false, false);
-insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('gbride1', 'cjenyns1@meetup.com', '3gL51qlWbl', 'optimize robust solutions', png, '05/06/1947', 'f', '735', true, false, false, false);
-insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('sdrowsfield2', 'sgiacovelli2@about.com', '7DtcamyRUu', 'streamline virtual web-readiness', png, '04/11/1943', 'f', '9917', true, false, false, false);
-insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('gallinson3', 'bferagh3@eepurl.com', 'U2hetHaRqiQH', 'unleash clicks-and-mortar platforms', png, '01/14/1919', 'n', '64397', false, false, false, false);
-insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('bwilloughway4', 'abilbery4@acquirethisname.com', 'Yf5q7m', 'iterate back-end channels', jpg, '06/22/2006', 'm', '2023', false, false, false, false);
-insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('ashemwell5', 'eelles5@unesco.org', 'SFYg55', 'transition magnetic infrastructures', png, '10/08/2002', 'n', '36', true, false, false, false);
-insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('fscading6', 'nsherrington6@arizona.edu', 'aJyJS5I', 'mesh value-added infrastructures', png, '04/19/1917', 'f', '34', false, false, false, true); --adicionar um user deleted
-insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('mmountcastle7', 'ncamier7@uol.com.br', 'KyhSWT4Ip4gV', 'engineer collaborative users', png, '01/20/1976', 'n', '89', false, true, false, false);
-insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('mplowman8', 'showgego8@psu.edu', '97HEdZhHmt', 'utilize seamless partnerships', jpg, '09/12/1932', 'n', '335', false, false, false, false);
-insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('lprozescky9', 'akittow9@1688.com', 'g1yng049V', 'brand interactive partnerships', png, '08/31/1963', 'm', '2', false, false, false, false);
-insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('abellinia', 'ayoulla@dropbox.com', 'Sh5JeW9', 'mesh revolutionary applications', gif, '08/17/1927', 'm', '34685', false, false, false, false);
-insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('cboordb', 'lwanneb@blogtalkradio.com', '7tDRyp8s', 'enable revolutionary systems', png, '12/22/1965', 'f', '24',false, true, false, false);
-insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('ttrembeyc', 'ssamarthc@aol.com', '1bNnW5YX', 'engineer clicks-and-mortar relationships', png, '12/13/1931', 'f', '109', false, false, false, false);
-insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('sstanfordd', 'nbrendeld@spiegel.de', '0qnuVL8Aj', 'envisioneer sexy users', gif, '07/01/1906', 'm', '77', false, false, false, false);
-insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('iyoudee', 'adrainse@goo.ne.jp', 'LOUT2zpl', 'reintermediate open-source methodologies', png, '09/24/1979', 'm', '78', false, false, false, false);
-insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('jwarfieldf', 'mervinef@behance.net', 'HN9mhx', 'incubate robust channels', jpg, '04/30/1935', 'f', '29489', false, false, false, false);
-insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('iodomg', 'bwashbrookg@bloglovin.com', 'ZdTpo2Atsf', 'benchmark collaborative content', png, '03/16/2013', 'f', '504', false, false, false, false); --não tem idade suficiente
-insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('sphonixh', 'sduchesneh@moonfruit.com', 'J4GQnyXC8C', 'aggregate revolutionary bandwidth', png, '06/05/1954', 'f', '2387', false, false, true, false); --partner com reputação inferior ao requerido
-insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('fosbidstoni', 'tdoddemeedei@umn.edu', '5PwEC1GpWoU', 'engage user-centric web-readiness', png, '10/08/2005', 'm', '28422', false, false, false, false);
-insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('bturrillj', 'jwarlowj@g.co', '3NRCEXNardY', 'enhance frictionless e-business', png, '11/11/1924', 'm', '105001', false, false, true, false);
+insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('mpitchers0', 'lpriestner0@tiny.cc', '13oVi9u1gN', 'morph distributed schemas', 'png', '02/20/1922', 'f', '01532', false, false, false, false);
+insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('gbride1', 'cjenyns1@meetup.com', '3gL51qlWbl', 'optimize robust solutions', 'png', '05/06/1947', 'f', '735', true, false, false, false);
+insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('sdrowsfield2', 'sgiacovelli2@about.com', '7DtcamyRUu', 'streamline virtual web-readiness', 'png', '04/11/1943', 'f', '9917', true, false, false, false);
+insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('gallinson3', 'bferagh3@eepurl.com', 'U2hetHaRqiQH', 'unleash clicks-and-mortar platforms', 'png', '01/14/1919', 'n', '64397', false, false, false, false);
+insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('bwilloughway4', 'abilbery4@acquirethisname.com', 'Yf5q7m', 'iterate back-end channels', 'jpg', '06/22/2006', 'm', '2023', false, false, false, false);
+insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('ashemwell5', 'eelles5@unesco.org', 'SFYg55', 'transition magnetic infrastructures', 'png', '10/08/2002', 'n', '36', true, false, false, false);
+insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('fscading6', 'nsherrington6@arizona.edu', 'aJyJS5I', 'mesh value-added infrastructures', 'png', '04/19/1917', 'f', '34', false, false, false, true); 
+insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('mmountcastle7', 'ncamier7@uol.com.br', 'KyhSWT4Ip4gV', 'engineer collaborative users', 'png', '01/20/1976', 'n', '89', false, true, false, false);
+insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('mplowman8', 'showgego8@psu.edu', '97HEdZhHmt', 'utilize seamless partnerships', 'jpg', '09/12/1932', 'n', '335', false, false, false, false);
+insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('lprozescky9', 'akittow9@1688.com', 'g1yng049V', 'brand interactive partnerships', 'png', '08/31/1963', 'm', '2', false, false, false, false);
+insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('abellinia', 'ayoulla@dropbox.com', 'Sh5JeW9', 'mesh revolutionary applications', 'gif', '08/17/1927', 'm', '34685', false, false, false, false);
+insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('cboordb', 'lwanneb@blogtalkradio.com', '7tDRyp8s', 'enable revolutionary systems', 'png', '12/22/1965', 'f', '24',false, true, false, false);
+insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('ttrembeyc', 'ssamarthc@aol.com', '1bNnW5YX', 'engineer clicks-and-mortar relationships', 'png', '12/13/1931', 'f', '109', false, false, false, false);
+insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('sstanfordd', 'nbrendeld@spiegel.de', '0qnuVL8Aj', 'envisioneer sexy users', 'gif', '07/01/1906', 'm', '77', false, false, false, false);
+insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('iyoudee', 'adrainse@goo.ne.jp', 'LOUT2zpl', 'reintermediate open-source methodologies', 'png', '09/24/1979', 'm', '78', false, false, false, false);
+insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('jwarfieldf', 'mervinef@behance.net', 'HN9mhx', 'incubate robust channels', 'jpg', '04/30/1935', 'f', '29489', false, false, false, false);
+insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('iodomg', 'bwashbrookg@bloglovin.com', 'ZdTpo2Atsf', 'benchmark collaborative content', 'png', '03/16/2013', 'f', '504', false, false, false, false); --não tem idade suficiente
+insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('sphonixh', 'sduchesneh@moonfruit.com', 'J4GQnyXC8C', 'aggregate revolutionary bandwidth', 'png', '06/05/1954', 'f', '2387', false, false, true, false); --partner com reputação inferior ao requerido
+insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('fosbidstoni', 'tdoddemeedei@umn.edu', '5PwEC1GpWoU', 'engage user-centric web-readiness', 'png', '10/08/2005', 'm', '28422', false, false, false, false);
+insert into users (username, email, password, description, photo, birthdate, gender, reputation, is_moderator, is_banned, is_partner, is_deleted) values ('bturrillj', 'jwarlowj@g.co', '3NRCEXNardY', 'enhance frictionless e-business', 'png', '11/11/1924', 'm', '105001', false, false, true, false);
 
 
 insert into follow (follower_id, users_id) values (6, 7);
@@ -41,8 +41,65 @@ insert into follow (follower_id, users_id) values (6, 16);
 insert into follow (follower_id, users_id) values (11, 1);
 insert into follow (follower_id, users_id) values (19, 20);
 
-insert into ban (users_id, moderator_id, end_date, reason) values (4, 8, '8/13/2020', null);
-insert into ban (users_id, moderator_id, end_date, reason) values (7, 5, '12/10/2022', null);
-insert into ban (users_id, moderator_id, end_date, reason) values (11, 18, '5/9/2021', null); 
-insert into ban (users_id, moderator_id, end_date, reason) values (20, 9, '7/1/2020', null); --data final < data inicial
-insert into ban (users_id, moderator_id, end_date, reason) values (14, 7, null, null);
+insert into ban (users_id, moderator_id, end_date, reason) values (4, 6, '8/13/2020', 'Racist comment');
+insert into ban (users_id, moderator_id, end_date, reason) values (7, 3, '12/10/2022', 'Plays fortnite');
+insert into ban (users_id, moderator_id, end_date, reason) values (11, 2, '5/9/2021', 'Hate speech'); 
+insert into ban (users_id, moderator_id, end_date, reason) values (20, 6, '7/1/2020', 'Marketed Ponzi scheme'); --data final < data inicial
+insert into ban (users_id, moderator_id, end_date, reason) values (14, 1, null, 'Used dangerous external link'); --não é moderador
+
+insert into content(author_id, title, nr_votes) values(5,'New Mexico, which has one of the highest poverty rates in the U.S., is a vaccination pacesetter thanks to decisive political decisions, homegrown technology and cooperation.',0);
+insert into content(author_id, title, nr_votes) values(12,
+'President Joe Biden said Tuesday that he plans to deliver “a lot” on police reform but would not elaborate further ahead of a meeting that afternoon with Vice President Kamala Harris and key members of the Congressional Black Caucus in the Oval Office.
+Biden, speaking days after police killed Daunte Wright, a 20-year-old Black man, in a Minneapolis suburb, said he would inform reporters of his plans to reform police at a later date.
+The White House billed Tuesday afternoon’s meeting with members of the CBC as an opportunity to create a path forward on voting rights, racial equity and infrastructure legislation. The meeting comes a few days after Susan Rice, director of the Domestic Policy Council, announced that the Biden administration was pausing the creation of a national police oversight commission.',5); --votes >0 e user banido
+insert into content(author_id, title, nr_votes) values(15,'MANILA (Reuters) - The Philippines filed fresh diplomatic protests to China on Wednesday after accusing its giant neighbour of undertaking illegal fishing and massing more than 240 boats within the Southeast Asian countrys territorial waters.
+
+The Philippine Department of Foreign Affairs said that two protests had been lodged, days after Manila summoned Chinese Ambassador Huang Xilian to press for the withdrawal of its vessels on the disputed Whitsun Reef in the South China Sea and other Philippine maritime zones.
+
+The Philippines last month described the presence of over 200 boats believed to be manned by militias inside its 200 mile (322 km) exclusive economic zone as "swarming and threatening", while the United States, Japan and others have voiced concern about China’s intentions, prompting rebukes by Beijing.
+
+In a Twitter post, Foreign Secretary Teodoro Locsin said: "they really are fishing everything in the water that belongs by law to us."
+
+A Philippine government taskforce said the vessels, which are about 60 metres (197 ft) in length, can catch a tonne of fish a day. It said 240 were in various areas in Philippine waters as of Sunday, including nine at Whitsun Reef.
+
+"The continuous swarming of Chinese vessels poses a threat to the safety of navigation, safety of life at sea, and impedes the exclusive right of Filipinos to benefit from the marine wealth in the EEZ," the task force said in a statement late on Monday.
+
+China embassy in Manila and the foreign ministry in Beijing did not immediately respond to requests for comment.',0);
+insert into content(author_id, title, nr_votes) values(5,'Life is difficult in North Korea but there is no famine and some cross-border shipments may resume soon, Russia ambassador in Pyongyang said, a week after North Korean leader Kim Jong Un declared the country was facing a "worst-ever situation."
+
+Kim last week urged ruling party officials to wage another “Arduous March” of work and sacrifice, linking the current economic crises to a period in the 1990s of famine and disaster.
+
+Russia ambassador, one of the few foreign envoys in the country, said that while it was unclear exactly what Kim meant the current situation could not be compared to that period.
+
+"Thank god, it is a long shot from the Arduous March, and I hope it would never come to that," Ambassador Alexander Matsegora told Russias TASS news agency according to a transcript published on Wednesday.
+',0);
+insert into content(author_id, title, nr_votes values(19, 'Man, North Korea is such a prison',0);
+insert into content(author_id, title, nr_votes values(20, 'ikr',0);
+insert into content(author_id, title, nr_votes values(15, 'My president <3',0);
+insert into content(author_id, title, nr_votes values(12, 'China being China',0);
+
+insert into tag (name) values('economy');
+insert into tag (name) values('politics');
+insert into tag (name) values('sports');
+insert into tag (name) values('covid');
+insert into tag (name) values('celebreties');
+insert into tag (name) values('music');
+
+insert into news(content_id,title, image, trending_score, nr_comment) values (1,'How New Mexico Became the State With the Highest Rate of Full Vaccinations','png',0,0);
+insert into news(content_id,title, image, trending_score, nr_comment) values (2,'Biden promises to deliver on police reform during meeting with Congressional Black Caucus','png',0,0);
+insert into news(content_id,title, image, trending_score, nr_comment) values (3,'Philippines files new diplomatic protests over Chinese boats in disputed waters','png',0,0);
+insert into news(content_id,title, image, trending_score, nr_comment) values (4,'Russian ambassador says no famine in North Korea, trade may resume soon','jpg',0,0);
+
+insert into news_tag(news_id, tag_id) values (1,2);
+insert into news_tag(news_id, tag_id) values (2,2);
+insert into news_tag(news_id, tag_id) values (3,2);
+insert into news_tag(news_id, tag_id) values (4,2);
+insert into news_tag(news_id, tag_id) values (5,2);
+insert into news_tag(news_id, tag_id) values (1,1);
+
+insert into comment(content_id, news_id,reply_to_id) values (1,4,null);
+insert into comment(content_id, news_id,reply_to_id) values (2,4,2);
+insert into comment(content_id, news_id,reply_to_id) values (3,2,null);
+insert into comment(content_id, news_id,reply_to_id) values (4,3,null);
+
+insert into request(from_id, moderator_id, reason, creation_date,status,revision_date) values (
