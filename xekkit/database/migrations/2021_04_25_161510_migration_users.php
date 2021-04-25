165121cfccff2d41fdbe9386900a1f8ad01fe0b3
Guilherme Calassi <guilherme@yourmove.com.br>
2021-04-25 17:35:45 +0100

    user migration

--- xekkit/database/migrations/2021_04_25_161510_migration_users.php
+++ xekkit/database/migrations/2021_04_25_161510_migration_users.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class MigrationUsers extends Migration
@@ -19,19 +20,21 @@ class MigrationUsers extends Migration
 
             $table->string('username', 20)->unique();
             $table->text('email')->unique();
-            $table->text('password')->unique();
-            $table->text('password')->unique();
-            $table->text('password')->unique();
-            $table->text('password')->unique();
-            $table->text('password')->unique();
-            $table->text('password')->unique();
-            $table->text('password')->unique();
-            $table->text('password')->unique();
-            $table->boolean('is_moderator');
-            $table->boolean('is_partner');
-            $table->boolean('is_banned');
-            $table->boolean('is_deleted');
+            $table->text('password');
+            $table->text('description');
+            $table->text('photo')->unique();
+            $table->date('birthdate');
+            $table->enum('gender', ['m', 'f', 'n']);
+            $table->integer('reputation')->default(0);
+            $table->date('last_day_of_vote')->nullable();
+            $table->integer('count_last_day_rep')->default(0)->nullable();
+            $table->boolean('is_moderator')->default(false);
+            $table->boolean('is_partner')->default(false);
+            $table->boolean('is_banned')->default(false);
+            $table->boolean('is_deleted')->default(false);
         });
+
+        DB::statement('ALTER TABLE users ADD CONSTRAINT chkreputation CHECK (reputation > 0);');
     }
 
     /**
