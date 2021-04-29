b7a85741695aec00656f6e326601a07054f26f0d
Guilherme Calassi <guilherme@yourmove.com.br>
2021-04-30 00:35:31 +0100

    start with news

--- xekkit/app/Http/Controllers/NewsController.php
+++ xekkit/app/Http/Controllers/NewsController.php
@@ -7,22 +7,35 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 
 use App\Models\News;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class NewsController extends Controller
 {
     /**
      * Shows the news for a given news_id.
      *
-     * @param  string  $id
-     * @return Response
+     * @param string $id
+     *
+     * @return \Illuminate\Contracts\View\View
      */
     public function show($id)
     {
-      return view('pages.news');
+        $news = DB::table('news')->where('content_id', $id)->first();
+        $comments = DB::table('comments')->where('news_id', $id)->get();
+        $comments_array = [];
+        foreach ($comments as $comment){
+
+        }
+        if (empty($news)) {
+            throw new NotFoundHttpException();
+        }
+
+        return view('pages.news', ['news' => $news]);
     }
 
 
     //----------------------------------------
+
     /**
      * Edit a news.
      *
@@ -31,22 +44,22 @@ class NewsController extends Controller
     public function edit(Request $request)
     {
 
-      $this->authorize('create', $news);
+        $this->authorize('create', $news);
 
-      $news->name = $request->input('name');
-      $news->news_id = Auth::news()->id;
-      $news->save();
+        $news->name = $request->input('name');
+        $news->news_id = Auth::news()->id;
+        $news->save();
 
-      return $news;
+        return $news;
     }
 
     public function delete(Request $request, $id)
     {
-      $news = News::find($id);
+        $news = News::find($id);
 
-      $this->authorize('delete', $news);
-      $news->delete();
+        $this->authorize('delete', $news);
+        $news->delete();
 
-      return $news;
+        return $news;
     }
 }
