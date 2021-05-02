843d095446bf5c4f813fd65d9967f9056a6422bc
Guilherme Calassi <guilherme@yourmove.com.br>
2021-05-02 18:29:07 +0100

    auth::check

--- xekkit/app/Policies/NewsPolicy.php
+++ xekkit/app/Policies/NewsPolicy.php
@@ -6,6 +6,7 @@ use App\Models\News;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
 use Illuminate\Auth\Access\Response;
+use Illuminate\Support\Facades\Auth;
 
 class NewsPolicy
 {
@@ -14,7 +15,8 @@ class NewsPolicy
     /**
      * Determine whether the user can view any models.
      *
-     * @param  \App\Models\User  $user
+     * @param \App\Models\User $user
+     *
      * @return mixed
      */
     public function viewAny(User $user)
@@ -25,21 +27,25 @@ class NewsPolicy
     /**
      * Determine whether the user can view the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\News  $news
+     * @param \App\Models\User $user
+     * @param \App\Models\News $news
+     *
      * @return mixed
      */
     public function view(User $user, News $news)
     {
-        if($news->content->author->is_banned || $news->content->author->is_deleted)
+        if ($news->content->author->is_banned || $news->content->author->is_deleted) {
             return Response::deny('The user that created this post is either banned or deleted.');
-        else return !($user->is_banned || $user->is_deleted);
+        } else {
+            return !(Auth::check() && (Auth::user()->is_banned || Auth::user()->is_deleted)) ? Response::allow() : Response::deny('You can not see any posts.');
+        }
     }
 
     /**
      * Determine whether the user can create models.
      *
-     * @param  \App\Models\User  $user
+     * @param \App\Models\User $user
+     *
      * @return mixed
      */
     public function create(User $user)
@@ -50,8 +56,9 @@ class NewsPolicy
     /**
      * Determine whether the user can update the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\News  $news
+     * @param \App\Models\User $user
+     * @param \App\Models\News $news
+     *
      * @return mixed
      */
     public function update(User $user, News $news)
@@ -62,8 +69,9 @@ class NewsPolicy
     /**
      * Determine whether the user can delete the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\News  $news
+     * @param \App\Models\User $user
+     * @param \App\Models\News $news
+     *
      * @return mixed
      */
     public function delete(User $user, News $news)
