fecf2092525981ea425db0aa87a124f012882144
Beatriz Mendes <45185351+biam05@users.noreply.github.com>
2021-05-09 17:49:26 +0100

    working on feed, finished recent, finished hot

--- xekkit/app/Http/Controllers/HomepageController.php
+++ xekkit/app/Http/Controllers/HomepageController.php
@@ -3,16 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+use App\Models\News;
+use App\Models\Content;
+use App\Models\Follow;
 
 class HomepageController extends Controller
 {
-    /**
-     * Shows the news for a given news_id.
-     *
-     * @return Response
-     */
-    public function show()
-    {
-      return view('pages.homepage');
+    
+    public function show(Request $request)
+    {      
+
+      $following = Auth::user()->following;
+      $feedPosts = array();
+      foreach($following as $user){
+        $contents = $user->contents;
+        foreach($contents as $content){
+          array_push($feedPosts, $content->new);
+        }
+      }
+
+      $recentPosts = News::all();
+      foreach($recentPosts as $recentPost){
+        $recentPost->content = $recentPost->content;
+      }
+
+      $recentPosts = $recentPosts->sortByDesc('content.date');      
+      $hotPosts = $recentPosts->sortByDesc('content.nr_votes');
+
+      return view('pages.homepage', [
+        'feedPosts' => $feedPosts,
+        'recentPosts'=> $recentPosts, 
+        'hotPosts'=> $hotPosts]);
     }
 }
