413ab31a18413f7d582e3175b30b7acad49543d7
Beatriz Mendes <45185351+biam05@users.noreply.github.com>
2021-05-11 11:30:06 +0100

    fixed bug in homepage feed

--- xekkit/app/Http/Controllers/HomepageController.php
+++ xekkit/app/Http/Controllers/HomepageController.php
@@ -13,43 +13,37 @@ use App\Models\Follow;
 
 class HomepageController extends Controller
 {
-    
+  
     public function show(Request $request)
     {      
 
-      $feedPosts = array();
+        $feedPosts = array(); 
+        
+        if(Auth::check()){
+            $feedPosts = News::whereIn('content_id', function ($query) {
+                $query->select('id')
+                    ->from('content')
+                    ->whereIn('author_id', function ($query) {
+                        $query->select('users_id')
+                            ->from('follow')
+                            ->where('follower_id', Auth::id());
+                    });
+            })->get();
+        }        
 
-      // if(Auth::check()){
-      //   $feedPosts = DB::select('
-      //   SELECT n.content_id, n.title, n.image, c.date, c.body, c.nr_votes, u.username, u.is_partner
-      //   FROM news n, content c, users u
-      //   WHERE n.content_id = c.id
-      //       AND c.author_id = u.id
-      //       AND u.is_deleted = \'false\'
-      //       AND u.is_banned = \'false\'
-      //       AND c.author_id IN (SELECT f.users_id FROM follow f WHERE f.follower_id = :my_users_id)
-      //   ORDER BY c.date DESC
-      //   ', ['my_users_id' => Auth::id()]);
+        $posts = News::all();
+        foreach($posts as $post){
+            $post->content = $post->content;
+        }
 
+        $recentPosts = $posts->sortByDesc('content.date');      
+        $hotPosts = $posts->sortByDesc('content.nr_votes');
+        $trendingPosts = $posts->sortByDesc('trending_score');
 
-      //   //dd(Auth::user()->username);
-      // }
-      
-      
-
-      $posts = News::all();
-      foreach($posts as $post){
-        $post->content = $post->content;
-      }
-
-      $recentPosts = $posts->sortByDesc('content.date');      
-      $hotPosts = $posts->sortByDesc('content.nr_votes');
-      $trendingPosts = $posts->sortByDesc('trending_score');
-
-      return view('pages.homepage', [
-        'trendingPosts' => $trendingPosts,
-        'feedPosts' => $feedPosts,
-        'recentPosts'=> $recentPosts, 
-        'hotPosts'=> $hotPosts]);
+        return view('pages.homepage', [
+            'trendingPosts' => $trendingPosts,
+            'feedPosts' => $feedPosts,
+            'recentPosts'=> $recentPosts, 
+            'hotPosts'=> $hotPosts]);
     }
 }
