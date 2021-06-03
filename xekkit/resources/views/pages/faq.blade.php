decb97e724415fe9b723b190b2fce642ad81517a
Guilherme Calassi <guilherme@yourmove.com.br>
2021-06-04 00:18:49 +0100

    faq page

--- xekkit/resources/views/pages/faq.blade.php
+++ xekkit/resources/views/pages/faq.blade.php
@@ -6,18 +6,18 @@
 
 <main class="container-xl text-light">
     <div class="row mb-3">
-        <h1 class="col-auto"><i class="fas fa-book"></i>     FAQ</h1>
+        <h1 class="col-auto"><i class="fas fa-book mx-4 mt-5"></i>FAQ</h1>
     </div>
 
     <div class="accordion accordion-flush" id="faq">
-        @each('partials.faq.faq_question', $topics, 'topic')        
+        @each('partials.faq.faq_question', $topics, 'topic')
     </div>
 
     @auth
         @if (Auth::user()->is_moderator)
             @include('partials.faq.add_question')
-        @endif   
-    @endauth   
+        @endif
+    @endauth
 </main>
 
 @endsection
